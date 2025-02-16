2cb69c46ea4be146c4f0b20ce71927a6540cd423
Shuvodip Das Himu <shuvodipdashimu@gmail.com>
2025-02-16 13:43:23 +0600

    first commit

---
+++ max3.js
@@ -0,0 +1,20 @@
+const jim = 256;
+const tim = 89;
+const kim = 168;
+
+if(jim > tim && jim > kim){
+  console.log('Jim is the ultimate boss');
+}
+else if(tim > jim && tim > kim){
+  console.log('Tim is the ultimate boss');
+}
+else {
+  console.log('Kim is the kardashians boss');
+}
+
+function maxOfThree(num1, num2, num3){
+  // do it yourself
+}
+
+const max = Math.max(12, 1, 56, 5, 65, 8 , 1, 99, 2);
+console.log(' max using Math.max', max);
